6bd020418c7716a3239ed746deacffa9eefa0c0f
yejiayuan <yejiayuan@fenlibao.com>
2019-07-18 17:52:30 +0800

    part3-2

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const Koa = require('koa')
-// 创建一个Koa对象表示web app本身:
-const app = new Koa()
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-  console.log('>>step 1')
-  await next()
-  console.log('<<step 1')
-})
-app.use(async (ctx, next) => {
-  console.log('>>step 2')
-  await next()
-  console.log('<<step 2')
-})
-app.use(async (ctx, next) => {
-  console.log('>>step 3')
-  await next()
-  console.log('<<step 3')
-})
-app.use(async (ctx, next) => {
-  console.log('>>step 4')
-  ctx.body='<h1>hello world!!!</h1>'
-  console.log('<<step 4')
-})
-// 在端口3000监听:
-app.listen(3000)
+const Koa = require('koa');
+const app = new Koa();
+app.use(async ctx => {
+  let _html = '404 NotFound'
+  switch (ctx.url) {
+    case '/':
+      _html = '<h1>Index</h1>';
+      break;
+    case '/adout':
+      _html = '<h1>About</h1>';
+      break;
+    case '/hello':
+      _html = '<h1>world</h1>';
+      break;
+    default:
+      break;
+  }
+  ctx.body = _html;
+});
+app.listen(3000);
