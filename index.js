54bb3efd4be2d491e4483077c521e6b524256fc3
yejiayuan <yejiayuan@fenlibao.com>
2019-07-18 09:59:26 +0800

    part 1:新建一个Koa的Web应用

---
+++ index.js
@@ -0,0 +1,39 @@
+// const koa = require('koa');
+// const app = new koa();
+// app.use(async function (ctx,next){
+//   console.log('>> one');
+//   await next();
+//   console.log('<< one');
+// });
+//
+// app.use(async function (ctx,next){
+//   console.log('>> two');
+//   await next();
+//   console.log('<< two');
+// });
+//
+// app.use(async function (ctx,next){
+//   console.log('>> three');
+//   await next();
+//   console.log('<< three');
+// });
+// app.use(async function (ctx){
+//   console.log('>> Hello World');
+//   console.log('>>',ctx.url);
+//   ctx.body = 'Hello World';
+// });
+//
+// app.listen(5427);
+//
+
+const Koa = require('koa');
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+  await next();
+  ctx.type = 'text/html';
+  ctx.body = '<h1>Hello World!</h1>';
+});
+// 在端口3000监听:
+app.listen(3000);
