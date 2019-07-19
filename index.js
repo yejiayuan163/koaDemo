a6d677e40e7845edbef4585166fd85c54ee7df4b
yejiayuan <yejiayuan@fenlibao.com>
2019-07-19 10:06:52 +0800

    sampleRouter

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ app.use(async ctx => {
   let _html = '404 NotFound'
   switch (ctx.url) {
     case '/':
-      _html = '<h1>Index</h1>';
+      _html = '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>Index</h1>';
       break;
-    case '/adout':
-      _html = '<h1>About</h1>';
+    case '/about':
+      _html = '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>About</h1>';
       break;
-    case '/hello':
-      _html = '<h1>world</h1>';
+    case '/world':
+      _html = '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>World</h1>';
       break;
     default:
       break;
