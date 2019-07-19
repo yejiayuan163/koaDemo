beaedc54dd9c2c579dcd432fc1b9f4c8ba684ec7
yejiayuan <yejiayuan@fenlibao.com>
2019-07-19 14:31:21 +0800

    koa-router

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = new Koa();
 
 const index = require('./routes/index');
 app.use(index.routes(), index.allowedMethods())
+
 app.listen(3000);
--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -8,12 +8,10 @@ const about = async (ctx, next) => {
   const _html =  index_service.about()
   ctx.body= _html
 }
-
 const world = async (ctx, next) => {
   const _html =  index_service.world()
   ctx.body= _html
 }
-
 module.exports = {
   index,
   about,
